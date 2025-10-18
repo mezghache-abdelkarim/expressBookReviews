57e95f3d58f04143c69fe5e084334b91c60c5fa8
mezghache.abdelkarim <mezghache.abdelkarim@gmail.com>
2025-10-18 00:49:55 -0400

    task5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -90,9 +90,22 @@ public_users.get('/title/:title', function (req, res) {
 
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+  // Step 1: Get the ISBN from the request parameters
+  const isbn = req.params.isbn;
+
+  // Step 2: Find the book in the database
+  const book = books[isbn];
+
+  // Step 3: Check if the book exists
+  if (book) {
+    // Step 4: Return the book's reviews
+    res.send(JSON.stringify(book.reviews, null, 4));
+  } else {
+    // Step 5: If not found, send error message
+    res.status(404).json({ message: "Book not found" });
+  }
 });
+
 
 module.exports.general = public_users;
