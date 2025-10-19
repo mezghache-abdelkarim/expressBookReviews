2e68d055134dcd7c078bf9cbe897dae3f3249099
mezghache.abdelkarim <mezghache.abdelkarim@gmail.com>
2025-10-19 18:25:50 -0400

    task 8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -17,6 +17,7 @@ const authenticatedUser = (username, password) => {
 
 // --------- LOGIN ROUTE (/customer/login) -----------
 regd_users.post("/login", (req, res) => {
+  console.log(req.body)
   const { username, password } = req.body;
 
   // Check if username and password are provided
@@ -51,8 +52,32 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const review = req.query.review; // review comes from query string
+
+  console.log(isbn,review)
+
+
+  // Check if user is logged in
+  if (!req.session.authorization || !req.session.authorization.username) {
+    return res.status(401).json({ message: "User not logged in" });
+  }
+
+  const username = req.session.authorization.username;
+
+  // Check if book exists
+  const book = books[isbn];
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  // Add or update the review
+  book.reviews[username] = review;
+
+  return res.status(200).json({
+    message: `Review for book ISBN ${isbn} by user ${username} added/updated successfully`,
+    reviews: book.reviews
+  });
 });
 
 module.exports.authenticated = regd_users;
